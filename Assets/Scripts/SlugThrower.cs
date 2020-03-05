3e3409f20905715a3de2b799306bd9febe015c77
Brian M. Foster <bfoster6@butlercc.edu>
2020-03-05 15:11:37 -0600

    Added audio and rapidfire to slugthrower

--- Assets/Scripts/SlugThrower.cs
+++ Assets/Scripts/SlugThrower.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class SlugThrower : MonoBehaviour
 {
 	private Transform bulletSpawn;
@@ -11,26 +12,53 @@ public class SlugThrower : MonoBehaviour
 
 	[SerializeField]
 	private float bulletSpeed = 50f;
+	
+	[Range(0.05f, 0.5f)]
+	[SerializeField]
+	private float interval = 0.1f;
+
+	[SerializeField]
+	private AudioClip shot;
+
+	private AudioSource aud;
+	private bool canFire = true;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         bulletSpawn = this.transform.GetChild(0);
+		aud = this.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0)) {
+		// move this to the PlayerController script at some point.
+        if(Input.GetKey(KeyCode.Mouse0)) {
 			Use();
 		}
     }
 
 	public void Use() {
-		Debug.Log("Pow!");
-		Rigidbody rb = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
-		rb.AddRelativeForce(Vector3.forward * bulletSpeed, ForceMode.Impulse);
-		Destroy(rb.gameObject, 3f);
+		if(canFire) {
+			Debug.Log("Pow!");
+			Rigidbody rb = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+			rb.AddRelativeForce(Vector3.forward * bulletSpeed, ForceMode.Impulse);
+			Destroy(rb.gameObject, 3f);
+
+			// randomize the audiosource pitch for variety.
+			aud.pitch = Random.Range(0.95f, 1.05f);
+			// play sound clip
+			aud.PlayOneShot(shot);
+			StartCoroutine(WaitToFire());
+		}
+	}
+
+	IEnumerator WaitToFire() {
+		canFire = false;
+		yield return new WaitForSeconds(interval);
+		canFire = true;
 	}
 }
