efa9ddb833ee2626d568dd0e553db7af2de5e483
Quentin D. Stuchlik <qstuchlik@butlercc.edu>
2020-03-09 15:17:06 -0500

    I added a gun!

---
+++ Assets/Scripts/GravtiyGun.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravtiyGun : MonoBehaviour, IItem
+{
+    public Transform rayCaster;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Use()
+    {
+        Debug.Log("Use");
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        Debug.DrawRay(rayCaster.position, transform.TransformDirection(Vector3.forward * 3), Color.green);
+        if (Physics.Raycast(rayCaster.position, transform.TransformDirection(Vector3.forward * 3), out hit, 3f))
+        {
+            Debug.Log("I shot " + hit.transform.gameObject.name);
+            Rigidbody rb = hit.transform.gameObject.GetComponent<Rigidbody>();
+
+
+            rb.AddForce(Vector3.forward * 100, ForceMode.Impulse);
+        }
+    }
+    public void AltUse()
+    {
+        Debug.Log("Alt Use");
+
+    }
+}
