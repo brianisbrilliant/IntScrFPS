8500d9bf6b840c8d4be87d51cf9952f271cadd50
Bryanna N. Baker <bbaker17@butlercc.edu>
2020-03-09 15:09:57 -0500

    Portal gun SetUp

---
+++ Assets/Scripts/PortalGun.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalGun : MonoBehaviour, IItem
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void Use(){
+        Debug.Log("Portal 1 aka Use()");
+
+    }
+    public void AltUse(){
+        Debug.Log("Portal 2 aka AltUse()");
+        
+    }
+}
