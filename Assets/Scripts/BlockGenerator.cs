70d490449e18cc6a7a1ae260cadffe55048bce6f
Dante M. Bonomini <dbonomini@butlercc.edu>
2020-03-09 15:03:27 -0500

    New scene and script

---
+++ Assets/Scripts/BlockGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockGenerator : MonoBehaviour, IItem
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Use(){
+        Debug.Log("Pow!! Main Use.");
+
+    }
+
+    public void AltUse(){
+        Debug.Log("Pow!! Alt Use.");
+        
+    }
+}
