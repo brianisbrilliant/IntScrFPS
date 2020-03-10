470377e7e2acb721fe78b96de5a55c371ea1440b
Dante Bonomini <bonomini_dante@yahoo.com>
2020-03-10 18:38:06 -0500

    block generator and test scene changes

--- Assets/Scripts/BlockGenerator.cs
+++ Assets/Scripts/BlockGenerator.cs
@@ -4,19 +4,44 @@ using UnityEngine;
 
 public class BlockGenerator : MonoBehaviour, IItem
 {
+    public GameObject BlockyBlock;
+    private Vector3 myMouse;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    public void Use(){
+    public void Use() {
         Debug.Log("Pow!! Main Use.");
 
+        RaycastHit hit;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit)){
+            if(hit.rigidbody != null){
+                Debug.Log("I got a wall!!!");
+                //instantiate cube
+                Instantiate(BlockyBlock, hit.point, Quaternion.identity);
+            }
+        }
     }
 
-    public void AltUse(){
+    public void AltUse() {
         Debug.Log("Pow!! Alt Use.");
-        
+
+        RaycastHit hit;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit)){
+            if(hit.collider.tag == "BlockyBlock"){
+                Debug.Log("Time to die BlockyBlock");
+                //instantiate cube
+                Destroy(hit.collider.gameObject);
+            }
+            else if(hit.collider.tag == "wall"){
+                Debug.Log("Cannot Kill Walls!!");
+            }
+        }
     }
 }
