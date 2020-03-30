ff18aa3489f6745a2a179c538f159335a7d5d621
Brian Foster <brianisbrilliant@gmail.com>
2020-03-30 14:51:24 -0500

    Added Picking up and dropping!

--- Assets/Scripts/IItem.cs
+++ Assets/Scripts/IItem.cs
@@ -1,6 +1,10 @@
 ﻿
+using UnityEngine;
+
 public interface IItem
 {
     void Use();
     void AltUse();
+    void Pickup(Transform hand);
+    void Drop();
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,23 +8,69 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private IItem heldItem;
 
+    public GameObject lastTouchedItem;
+
+	private Transform hand;
+
+    private bool canPickup = false;
+
     // Start is called before the first frame update
     void Start()
     {   
-        // delete this after creating pickup functionality.
-        heldItem = this.transform.GetChild(0).GetChild(0).GetComponent<IItem>();
-        this.GetComponent<Rigidbody>().useGravity = false;
+        // This is the position you want to hold items at.
+    	hand = this.transform.GetChild(0).GetChild(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Mouse0)) {
-			heldItem.Use();
-		}
-        if(Input.GetKey(KeyCode.Mouse1)) {
-			heldItem.AltUse();
+		if(heldItem != null) {
+			if(Input.GetKey(KeyCode.Mouse0)) {
+				heldItem.Use();
+			}
+			if(Input.GetKey(KeyCode.Mouse1)) {
+				heldItem.AltUse();
+			}
+			if(Input.GetKey(KeyCode.Q)){
+				heldItem.Drop();
+				heldItem = null;
+			}
 		}
 
+		if(Input.GetKey(KeyCode.E)) {
+			Pickup();
+		}
+	}
+
+	void Pickup() {
+		if(canPickup) {
+			Debug.Log("lasttoucheditem = " + lastTouchedItem);
+			// what if we are already holding an item? (drop it!)
+			if(heldItem != null) {
+				heldItem.Drop();
+				heldItem = null;
+			}
+			Debug.Log("I am trying to pick up an item.");
+			heldItem = lastTouchedItem.GetComponent<IItem>();
+			//lastTouchedItem = null;
+			heldItem.Pickup(hand);
+		}
+	}
+
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Item")) {
+            Debug.Log("I can pickup an item");
+            canPickup = true;
+            lastTouchedItem = other.gameObject;
+			Debug.Log("lasttoucheditem = " + lastTouchedItem);
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(other.gameObject.CompareTag("Item")) {
+            Debug.Log("I can not pickup an item");
+            canPickup = false;
+            lastTouchedItem = null;
+        }
     }
 }
